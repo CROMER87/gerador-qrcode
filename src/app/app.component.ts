376b284e89869e03b0d6d38a4fc4ed9909bd23be
Romero <mero.rjf@hotmail.com>
2025-02-26 23:40:33 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { BoxComponent } from "./components/box/box.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, BoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/box/box.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import * as QRCode from 'qrcode';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-box',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './box.component.html',
+  styleUrl: './box.component.css'
+
+
+})
+export class BoxComponent {
+text!: string;
+qrcode!: string;
+
+async onSubmit(){
+  this.qrcode = await this.generateQrCode(this.text);
+  console.log(this.text);
+  this.text = '';
+}
+
+
+
+
+  async generateQrCode(text: string): Promise<string> {
+    try {
+      return await QRCode.toDataURL(text);
+    } catch (error) {
+      console.error('Erro ao gerar o QR Code:', error);
+      alert('Por favor, insira um texto válido.');
+      throw error;
+    }
+  }
+
+   salvarQrCode(): void {
+    if (this.qrcode) {
+      const link = document.createElement('a');
+      link.href = this.qrcode;
+      link.download = 'qrcode.png';
+      link.click();
+    } else {
+      alert('Por favor, gere um QR Code primeiro.');
+    }
+  }
+}
+
+
+
+
+
+
